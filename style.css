91211a5d7c56d6859fdddabba44b4e3efae2ea27
labgeek44 <spolekstudentu@proton.me>
2025-10-19 11:01:21 +0200

    Initial file creation

---
+++ style.css
@@ -0,0 +1,26 @@
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+
+#board {
+  width: 400px;
+  height: 400px;
+  background-color: #ccc;
+  border: 1px solid #333;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.tile {
+  width: 100px;
+  height: 100px;
+  box-sizing: border-box;
+  border: 1px solid #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2em;
+  font-weight: bold;
+}
